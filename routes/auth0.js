de63c88208e0d7af23201bd0c86fc1d512a7fecc
tphollis <thomas.preston.hollis@outlook.com>
2022-07-13 17:57:26 -0600

    Auth0 get profile

---
+++ routes/auth0.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { requiresAuth } = require('express-openid-connect');
+
+router.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
